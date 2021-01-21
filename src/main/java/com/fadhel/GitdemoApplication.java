b0db558d749393167952de219ea46f401055ad75
Fadhel <maalem.fadhel@gmail.com>
2021-01-21 14:29:27 +0100

    add hello world

--- src/main/java/com/fadhel/GitdemoApplication.java
+++ src/main/java/com/fadhel/GitdemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitdemoApplication.class, args);
+		SpringApplication.run(GitdemoApplication.class, args);System.out.print("hello world");
 	}
 
 }
